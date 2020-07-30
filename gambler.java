92afcacdf676e38b11276f9a316e96176ab9786e
sujit gargate <sujitgargate5893@gmail.com>
2020-07-30 10:05:13 +0530

    Initialization of varialbes and method newDay() added

---
+++ gambler.java
@@ -0,0 +1,14 @@
+public class uc1{
+
+	public int newDay(){
+                int stake=100;
+                return stake;
+	}
+	
+	
+
+	public static void main(String args[]){
+	int bet=1;
+	newDay();
+	}
+}
