3d8a60074e37bd80c1ea04dc1406c325f860fe58
sujit gargate <sujitgargate5893@gmail.com>
2020-07-30 10:10:57 +0530

    Checking for 50% win or loose

--- gambler.java
+++ gambler.java
@@ -1,20 +1,27 @@
-public class uc2{
+public class uc3{
 
-	public static int winLoose(){
-		int bet=1;
-		int stake=100;
-			int betting=(int)(Math.floor(Math.random()*10)%2);
-			if(betting==1){
-				stake++;
+        public static int winLoose(){
+                int bet=1;
+                int stake=100;
+                if(stake!=150 || stake!=50){
+                        int betting=(int)(Math.floor(Math.random()*10)%2);
+                        if(betting==1){
+                                stake+=50;
+                        }else{
+                                stake-=50;
 			}
-				stake--;
-		
-		return stake;
-	}
-	public static void main(String args[]){
-		int result=winLoose();
-		System.out.println(result);
-	}
+                }
+                if(stake==150){
+                        System.out.println("You won the game..You are done for today..!! Thank You..");
+                }else{
+                        System.out.println("You lost the game..You are done for today..!! please try again tomorrow..");
+		}
+                return stake;
+        }
+        public static void main(String args[]){
+                int result=winLoose();
+                System.out.println(result);
+        }
 
 
 }
