9aa63f9d217fcc72b912a0fa154611d528d73fb5
sujit gargate <sujitgargate5893@gmail.com>
2020-07-31 13:26:16 +0530

    Implemented: Asking user for continue playing for next month or Stop

--- gambler.java
+++ gambler.java
@@ -1,64 +1,17 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class gambler {
-	//Variable declarations
-    int stake=100;
-    int bet=1;
-    int unLuckiestDayOfMonth=0;
-    int LuckiestDayOfMonth=0;
-    Random rand = new Random();
-
-    public static void main(String args[]){
-        gambler obj=new gambler();
-        int daysToBePlayed=30; //Days to be Played in Gambling
-        int dailyGamblingLimit=50;
-        obj.findDaysWonLost(daysToBePlayed,dailyGamblingLimit);
-    }
-
-	//Method for Finding days won and lost in Month by how much
-   public void findDaysWonLost(int daysToBePlayed,int dailyGamblingLimit){
-		int maxWinningAmmount=0;
-      int maxLoosingAmmount=0;
-      for(int i=1;i<=daysToBePlayed;i++){
-
-			//Putting bet using Random()
-      	boolean bettingInCasino = rand.nextBoolean();
-         if(bettingInCasino==true){
-				//For calculating specific day's win
-	         for(int j=0;j<rand.nextInt(dailyGamblingLimit);j++) {
-	            stake += bet;
-                }
-						//Temp variable will be used for finding difference between 
-						//current cash and original stake value
-                int temp=stake-100;
-                System.out.println("Day "+i+ " winning is "+(temp));
-
-			//if current cash in temp is higher which means it's winning
-         if(temp>=maxWinningAmmount) {
-         	maxWinningAmmount=temp;
-            LuckiestDayOfMonth=i;
-			}
-			stake=100;
-
-			//bettingInCasino==false
-			}else{
-
-					//For calculating specific day's loose
-			for(int j=0;j<rand.nextInt(dailyGamblingLimit);j++) {
-				stake -= bet;
-			}
-         int temp=100-stake;
-         System.out.println("Day "+i+ " loosing is "+(temp));
-			//if current cash in temp is lower which means it's loosing
-          if(temp>=maxLoosingAmmount){
-          	maxLoosingAmmount=temp;
-            unLuckiestDayOfMonth=i;
-           }
-           stake=100;
-			}
-		}
-		System.out.println("You have lost "+maxLoosingAmmount+ " on Day" +unLuckiestDayOfMonth);
-      System.out.println(" You have Won  "+maxWinningAmmount+ " on Day "+LuckiestDayOfMonth);
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\nChoose option 1 if You would like to continue playing next month  \n choose option 2 if you wish to stop playing");
+        int ch=sc.nextInt();
+        switch (ch) {
+            case 1:
+                System.out.println("See You Next Month..");
+                break;
+            case 2:
+                System.out.println(" Bye Bye ,Come Again...!");
+        }
     }
 }
 
