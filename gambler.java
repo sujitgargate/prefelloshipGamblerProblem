48405d5fab5ae28d8820c23d1a68ae8ebff835ec
sujit gargate <sujitgargate5893@gmail.com>
2020-07-31 11:10:28 +0530

    UC1 code added

---
+++ gambler.java
@@ -0,0 +1,14 @@
+public class gambler{
+   int bet=1;
+	public static void main(String args[]){
+      gambler obj=new gambler();
+		obj.startingNewDay();
+	}
+}
+   public int startingNewDay(){
+      int stake=100;
+      return stake;
+   }
+
+
+
