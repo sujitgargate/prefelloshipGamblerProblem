fafaee5137652864a186dae6466ea988ba831598
sujit gargate <sujitgargate5893@gmail.com>
2020-07-31 12:18:03 +0530

    Implemented : Total Winning and Loosing in 20 days

--- gambler.java
+++ gambler.java
@@ -1,42 +1,37 @@
 import java.util.Random;
 public class gambler{
-   //Global variables declared
-   public static int bet = 1;
-   public static int stake = 100;
+	
+	//Constants declared
+	public static int daysToBeCalculated=20; //Total Days to Be calculated
+	public static int stake=100;
+	public static int bet=1;
+	public static int wonCounter=0; 
+	public static int looseCounter=0;
 
 	public static void main(String args[]){
-		System.out.println("New Day Stake is "+startingNewDay());
-
-   	int resultOfWinLoose = winLooseGame();
-      System.out.println(resultOfWinLoose);
-      }
-
-	//Method for finding winLooseGame
-	public static int winLooseGame(){
-	Random rand = new Random();
-
-	//Checking Whethwer stake value should not go above 150 or below 50
-   while(stake<=150 && stake >=50) {
-
-		//Random is used for generating boolean values
-   	boolean betting = rand.nextBoolean();
-      if (betting == true) {
-      stake += bet;
-      }else {
-      	stake -= bet;
-      }
-	}
-	//If stake reaches to 150 then He is winner for day and he can only Play on next day
-   if(stake == 150){
-   	System.out.println("You won the game..You are done for today..!! Thank You.. please try again tomorrow..");
-   }else{
-   	System.out.println("You lost the game..You are done for today..!! please try again tomorrow..");
-	}
-	return stake;
+		//Method calling
+		calculateTotalWinLoose(daysToBeCalculated);
 	}
 
-   public static int startingNewDay(){
-      int stake=100;
-		return stake;
-   }
+	public static void calculateTotalWinLoose(int daysToBeCalculated){
+		for(int i=0;i<daysToBeCalculated;i++){
+			findWinLoose();
+		}
+	}
+
+	//This method will Print the Number of wins or looses
+	public static void findWinLoose(){
+
+		Random rand = new Random();
+		//For Finding Winning or loosing using Random() function
+		boolean bettingInCasino = rand.nextBoolean();
+		if(bettingInCasino==true){
+			wonCounter++;
+			System.out.println("Winning "+wonCounter);
+		}else{
+			looseCounter--;
+			System.out.println("Loosing "+looseCounter);
+		}
+	}
 }
+
