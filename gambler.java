e4eee113029d566d196b1d69276bcdd679d1343f
sujit gargate <sujitgargate5893@gmail.com>
2020-07-31 11:53:20 +0530

    Implemented : Player Can not go below 50 or above 150

--- gambler.java
+++ gambler.java
@@ -1,27 +1,37 @@
-public class uc3{
+import java.util.Random;
+public class uc3 {
 
-        public static int winLoose(){
-                int bet=1;
-                int stake=100;
-                if(stake!=150 || stake!=50){
-                        int betting=(int)(Math.floor(Math.random()*10)%2);
-                        if(betting==1){
-                                stake+=50;
-                        }else{
-                                stake-=50;
-			}
-                }
-                if(stake==150){
-                        System.out.println("You won the game..You are done for today..!! Thank You..");
-                }else{
-                        System.out.println("You lost the game..You are done for today..!! please try again tomorrow..");
-		}
-                return stake;
+	//Global variables declared
+	public static int bet = 1;
+   public static int stake = 100;
+	
+   public static void main(String args[]){
+   	int resultOfWinLoose = winLooseGame();
+      System.out.println(resultOfWinLoose);
+      }
+
+	//Method for finding winLooseGame
+	public static int winLooseGame(){
+	Random rand = new Random();
+
+	//Checking Whethwer stake value should not go above 150 or below 50
+   while(stake<=150 && stake >=50) {
+
+		//Random is used for generating boolean values
+   	boolean betting = rand.nextBoolean();
+      if (betting == true) {
+      stake += bet;
+      }else {
+      	stake -= bet;
+      }
+	}
+				//If stake reaches to 150 then He is winner for day and he can only Play on next day
+            if(stake == 150){
+                System.out.println("You won the game..You are done for today..!! Thank You.. please try again tomorrow..");
+            }else{
+                System.out.println("You lost the game..You are done for today..!! please try again tomorrow..");
+            }
+            return stake;
         }
-        public static void main(String args[]){
-                int result=winLoose();
-                System.out.println(result);
-        }
+    }
 
-
-}
